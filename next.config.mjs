97f14d335161331f748fc1ffecbddde78da03bf2
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-06-13 08:47:18 +0330

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,11 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: 'lovely-flamingo-139.convex.cloud'
+          hostname: 'kindred-canary-966.convex.cloud'
         },
         {
           protocol: 'https',
-          hostname: 'sleek-capybara-771.convex.cloud'
+          hostname: 'kindred-canary-966.convex.cloud'
         },
         {
           protocol: 'https',
